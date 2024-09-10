c159d895402d19f57e4c0342073c5e94ffafd057
Jan Greń <jaxyt550@gmail.com>
2024-09-10 19:43:52 +0200

    init

---
+++ api/build.gradle.kts
@@ -0,0 +1,29 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.0.20"
+    id("io.ktor.plugin") version "2.3.12"
+}
+
+group = "com.adam_and_jan"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.adam_and_jan.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
