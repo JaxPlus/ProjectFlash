abf9d5e60a7092f475115c99693b5cc9c29795ef
Jan Greń <jaxyt550@gmail.com>
2024-09-13 21:57:29 +0200

    feat: started main page; added color palette to style.css; added Navbar.vue and IconButton.vue

---
+++ ui/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+﻿<script setup lang="ts">
+import IconButton from "./IconButton.vue";
+</script>
+
+<template>
+    <nav>
+        <IconButton>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="#161A1D" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 8h12M6 12h12M6 16h12"/></svg>
+        </IconButton>
+        <IconButton>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="#161A1D" d="M5.85 17.1q1.275-.975 2.85-1.537T12 15t3.3.563t2.85 1.537q.875-1.025 1.363-2.325T20 12q0-3.325-2.337-5.663T12 4T6.337 6.338T4 12q0 1.475.488 2.775T5.85 17.1M12 13q-1.475 0-2.488-1.012T8.5 9.5t1.013-2.488T12 6t2.488 1.013T15.5 9.5t-1.012 2.488T12 13m0 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22"/></svg>
+        </IconButton>
+    </nav>
+</template>
+
+<style scoped>
+    nav {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        border-bottom: var(--primary-color) 1px solid;
+    }
+</style>
