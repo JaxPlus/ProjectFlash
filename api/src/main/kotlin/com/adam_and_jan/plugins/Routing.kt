f4ca453dd975b4e423aac8b3fc7084605a9b2ff0
Jan Greń <jaxyt550@gmail.com>
2024-09-18 21:45:55 +0200

    Revert "feat: added cors"
    This reverts commit b17b9f50cf1e489fe6f540cdff63b9de5ddc7524.

--- api/src/main/kotlin/com/adam_and_jan/plugins/Routing.kt
+++ api/src/main/kotlin/com/adam_and_jan/plugins/Routing.kt
@@ -1,6 +1,5 @@
 package com.adam_and_jan.plugins
 
-import io.ktor.http.ContentType
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,10 +7,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText(
-                contentType = ContentType.parse("text/html; charset=utf-8"),
-                text = "haj"
-            )
+            call.respondText("Hello World!")
         }
     }
 }
