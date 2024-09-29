23dc406e500d9c5afc550a66fecfc456611addc9
Jan Greń <jaxyt550@gmail.com>
2024-09-29 18:31:03 +0200

    feat: added plugins to connect to db

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,7 @@
 val kotlin_version: String by project
 val logback_version: String by project
+val postgres_version: String by project
+val h2_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.20"
@@ -23,6 +25,10 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("com.h2database:h2:$h2_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
