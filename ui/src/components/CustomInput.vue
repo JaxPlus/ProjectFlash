29d3ff891c508fa45daa346e022153c346401a3a
Jan Greń <jaxyt550@gmail.com>
2024-10-04 21:49:21 +0200

    feat: added shadcn/ui-vue, tailwind; updated site css

---
+++ ui/src/components/CustomInput.vue
@@ -0,0 +1,18 @@
+﻿<script setup lang="ts">
+
+type htmlInputTypes = "text" | "password" | "button" | "checkbox" | "color" | "date" | "number" | "datetime-local" | "email" | "file" | "month" | "radio" | "range" | "reset" | "search" | "submit" | "tel" | "url" | "week"
+
+const props = defineProps<{
+    type: htmlInputTypes
+}>()
+</script>
+
+<template>
+    <input :type="props.type" />
+</template>
+
+<style scoped>
+    input {
+        
+    }
+</style>
