1e52f4a6a0d0caf562182705d3e95ccdc70b25e6
Jan Greń <jaxyt550@gmail.com>
2024-11-18 23:36:48 +0100

    feat: new profile page

---
+++ ui/src/pages/ProfilePage.vue
@@ -0,0 +1,28 @@
+﻿<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-full flex justify-center items-center p-7">
+        <div class="bg-secondary-color min-h-screen min-w-[55rem] max-w-[75rem] rounded-2xl mr-7">
+            <div class="w-full flex ">
+                <img src="../assets/2d54f86eaf5cbf4df14dee5bd62c8538.jpg" class="rounded-2xl h-[12rem] w-[12rem] m-7"/>
+                <div class="flex justify-center items-start flex-col">
+                    <h2 class="text-5xl font-bold">Jasiek Greń</h2>
+                    <span>JaxPlus</span>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+        <div class="bg-secondary-color min-h-screen min-w-[30rem] max-w-[45rem] rounded-2xl">
+            <h3 class="text-4xl text-start m-7">Ostatnio grane gry</h3>
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
