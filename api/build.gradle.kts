e8f3280d9cccc592789850c1b6cc9add4e7ad339
Jan Greń <jaxyt550@gmail.com>
2024-11-20 22:04:57 +0100

    feat: updated ProfilePage.vue; added dotenv-kotlin and dotenv for js

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,6 +2,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 val postgres_version: String by project
 val h2_version: String by project
+val dotenv_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.20"
@@ -28,6 +29,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_version")
     implementation("org.postgresql:postgresql:$postgres_version")
     implementation("com.h2database:h2:$h2_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
