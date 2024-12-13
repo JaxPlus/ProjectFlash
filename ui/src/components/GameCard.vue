aabffee848ffd3c2c1284102a5c9aba18cc0f748
Kannabiz <135163146+Kannabiz@users.noreply.github.com>
2024-12-13 16:22:38 +0100

    started working on game page, added redirecting to the game page after clicking on a game card

--- ui/src/components/GameCard.vue
+++ ui/src/components/GameCard.vue
@@ -1,13 +1,19 @@
 ﻿<script setup lang="ts">
 
+import {changePage} from "@/utility.ts";
+
 const props = defineProps<{
     title: string
 }>()
 
+function goToGame() {
+  changePage('/games')
+}
+
 </script>
 
 <template>
-    <div class="h-[10rem] min-w-[18rem] mx-2 flex items-end justify-center rounded-2xl bg-secondary-color z-5">
+    <div class="h-[10rem] min-w-[18rem] mx-2 flex items-end justify-center rounded-2xl bg-secondary-color z-5" @click="goToGame()">
         <p class="p-2 w-full rounded-b-2xl opacity-0 transition ease-in-out bg-gradient-to-t from-black">{{ title }}</p>
     </div>
 </template>
--- ui/src/router.ts
+++ ui/src/router.ts
@@ -4,12 +4,14 @@ import HelloWorld from "./pages/MainPage.vue";
 import ProfilePage from "@/pages/ProfilePage.vue";
 import LoginPage from "@/pages/LoginPage.vue";
 import RegisterPage from "@/pages/RegisterPage.vue";
+import GamePage from "@/pages/GamePage.vue";
 
 const routes = [
     { path: '/', component: HelloWorld },
     { path: '/login', component: LoginPage },
     { path: '/register', component: RegisterPage },
     { path: '/profile', component: ProfilePage },
+    { path: '/game', component: GamePage },
 ]
 
 const router = createRouter({
