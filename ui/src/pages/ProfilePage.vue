e7b9abbdc3123be3fa4ca612374b8e81b1a202c8
Jan Greń <jaxyt550@gmail.com>
2024-12-27 21:28:23 +0100

    feat: added icons to settings

--- ui/src/pages/ProfilePage.vue
+++ ui/src/pages/ProfilePage.vue
@@ -9,7 +9,6 @@ userStore.getUser()
 
 <template>
     <div class="w-full h-full grid grid-cols-2 gap-2 justify-center py-7 px-36">
-<!--        <div class="bg-secondary-color min-h-[calc(100vh+4rem)] min-w-[55rem] max-w-[75rem] rounded-2xl mr-7">-->
         <div class="bg-secondary-color min-h-[calc(100vh+4rem)] min-w-[30rem] max-w-[75rem] rounded-2xl mr-7">
             <div class="w-full flex">
                 <img src="../assets/2d54f86eaf5cbf4df14dee5bd62c8538.jpg" class="rounded-full h-[8rem] w-[8rem] m-7"/>
@@ -26,7 +25,6 @@ userStore.getUser()
             <div class="bg-secondary-color flex items-center min-h-28 text-start rounded-2xl">
                 <router-link class="px-8" to="/settings">Settings/Dashboard</router-link>
             </div>
-            <!--        <div class="bg-secondary-color min-h-[calc(100vh+4rem)] min-w-[30rem] max-w-[45rem] rounded-2xl">-->
             <div class="bg-secondary-color min-h-[calc(100vh+4rem)] rounded-2xl">
                 <h3 class="text-4xl text-start pt-4 mx-4 my-4">Ostatnio grane gry</h3>
                 <div class="grid grid-cols-2 justify-items-center">
@@ -38,7 +36,3 @@ userStore.getUser()
         </div>
     </div>
 </template>
-
-<style scoped>
-
-</style>
