73e80aab91c7ff5101919a68355fe0f423e9b027
Jan Greń <jaxyt550@gmail.com>
2025-01-12 19:04:19 +0100

    fix: changed how skeleton displays to line it up to game

--- ui/src/pages/GamePage.vue
+++ ui/src/pages/GamePage.vue
@@ -21,7 +21,7 @@ setTimeout(() => {
     player.style.height = "100%";
     player.style.width = "100%";
     document.getElementById("container")?.appendChild(player)
-    
+
     player.load("../src/assets/games/" + gameStore?.game?.gamePath);
     console.log(player.loadedConfig)
     isLoading.value = false;
@@ -32,24 +32,26 @@ setTimeout(() => {
 
 <template>
     <div class="w-full h-full">
-        <Skeleton :class="`w-[46rem] h-[35rem] ${isLoading ? 'block' : 'hidden'}`" />
-        <div :class="` ${isLoading ? 'hidden' : 'block'}`">
-            <div class="w-full flex flex-col items-center">
-               <div class="w-[46rem] h-[35rem] m-8" id="container"></div>
+        <div class="w-full flex flex-col items-center">
+            <Skeleton :class="`w-[46rem] h-[35rem] m-8 ${isLoading ? 'block' : 'hidden'}`"/>
+            <div :class="` ${isLoading ? 'hidden' : 'block'}`">
+                <div class="w-[46rem] h-[35rem] m-8" id="container"></div>
             </div>
-          <div class="w-full flex flex-row p-10">
-            <div class="bg-secondary border border-primary flex flex-col items-center min-h-64 text-start rounded-2xl">
-                <p>{{ gameStore?.game?.title }}</p>
-                <p>{{ gameStore?.game?.description }}</p>
+            <div class="w-full flex flex-row p-10">
+                <div
+                    class="bg-secondary border border-primary flex flex-col items-center min-h-64 text-start rounded-2xl">
+                    <p>{{ gameStore?.game?.title }}</p>
+                    <p>{{ gameStore?.game?.description }}</p>
+                </div>
+                <div
+                    class="bg-secondary border border-primary flex flex-col items-center w-2/5 min-w-min min-h-64 ml-10 rounded-2xl">
+                    <GameCard title="TEST" :game-id="1"/>
+                    <GameCard title="TEST" :game-id="2"/>
+                    <GameCard title="TEST" :game-id="3"/>
+                    <GameCard title="TEST" :game-id="4"/>
+                    <GameCard title="TEST" :game-id="5"/>
+                </div>
             </div>
-            <div class="bg-secondary border border-primary flex flex-col items-center w-2/5 min-w-min min-h-64 ml-10 rounded-2xl">
-                <GameCard title="TEST" game-id=id />
-                <GameCard title="TEST" game-id=id />
-                <GameCard title="TEST" game-id=id />
-                <GameCard title="TEST" game-id=id />
-                <GameCard title="TEST" game-id=id />
-            </div>
-          </div>
         </div>
     </div>
 </template>
