56037e7d592af164c38db52b81ceb013ef9c2959
Kannabiz <135163146+Kannabiz@users.noreply.github.com>
2025-01-13 21:43:26 +0100

    created TagPage.vue

--- ui/src/components/Menu.vue
+++ ui/src/components/Menu.vue
@@ -27,9 +27,8 @@ onMounted(async () => {
                 <svg class="fill-primary" xmlns="http://www.w3.org/2000/svg" width="0.75em" height="1em" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7L86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256L41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3l105.4 105.3c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256z"/></svg>
             </IconButton>
             <div class="flex flex-col items-center">
-                <TagMenuItem v-for="tag in tagsGames" :name="tag.tagName" />
+                <TagMenuItem v-for="tag in tagsGames" :name="tag.tagName" :tagId="tag.id.toString()" @click="$emit('closeMenu')"/>
             </div>
-<!--          <p v-for="tag in tagsGames">{{tag.tagName}}</p>-->
         </div>
     </div>
 </template>
---
+++ ui/src/pages/TagPage.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import {useGameStore} from "@/stores/GameStore.ts";
+import {onMounted, ref, watch} from "vue";
+import Game from "@/models/Game.ts";
+import GameCard from "@/components/GameCard.vue";
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+let id = route.params.tagId;
+let name = route.params.tagName;
+
+const gameStore = useGameStore();
+
+const allGames = ref<Game[]>([]);
+const isLoading = ref(false);
+
+watch(
+    () => route.params.tagId,
+    async (newId) => {
+      id = route.params.tagId;
+      allGames.value = await gameStore.getAllGamesByTag(newId.toString());
+    }
+)
+
+onMounted(async () => {
+  isLoading.value = true;
+
+  allGames.value = await gameStore.getAllGamesByTag(id.toString());
+
+  isLoading.value = false;
+})
+
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-[20rem] bg-primary">
+    <p class="text-5xl font-semibold">Category: {{name}}</p>
+  </div>
+  <br>
+  <div class="flex justify-center items-center w-full gap-4 flex-wrap">
+      <GameCard v-for="game in allGames" :title="game.title" :game-id="game.id"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
